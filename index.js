77ac1b04a8534781a1a709153b882641be9cf4be
ankitkumar2510 <60805888+ankitkumar2510@users.noreply.github.com>
2022-05-21 13:55:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+
+var numberOfDrumsKit = document.querySelectorAll(".drum").length;
+
+
+// Button Listen
+
+for(var i=0; i<numberOfDrumsKit; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        var buttonSelected = this.innerHTML;
+        makeSound(buttonSelected);
+        addAnimation(buttonSelected);
+    });
+}
+
+// KeyPress Listen
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    addAnimation(event.key);
+})
+
+function makeSound(key){
+    switch (key){
+        case ("w") :
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case ("a") :
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case ("s") :
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case ("d") :
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case ("j") :
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case ("k") :
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case ("l") :
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default : console.log("Not Listed!!!");
+    }
+}
+
+function addAnimation(key){
+    var element = document.querySelector("."+key);
+    if (element !== null){
+        element.classList.add("pressed");
+
+        setTimeout(function (){
+            element.classList.remove("pressed");
+        },100);
+    }
+}
+
+// function getClicked(){
+//     var audio = new Audio("sounds/tom-1.mp3");
+//     audio.play();
+//     // console.log(this);
+// }
